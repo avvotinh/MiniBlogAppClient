f0c891a245644a08109e288aa99572df9cdc70d5
Le Van Hop <hop.le@axonactive.com>
2021-07-07 16:54:48 +0700

    Update

--- src/Comment/CommentList.js
+++ src/Comment/CommentList.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        `http://localhost:4001/posts/${postId}/comments`
-      );
-
-      setComments(res.data);
-    };
-
-    fetchData();
-  }, [postId]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
